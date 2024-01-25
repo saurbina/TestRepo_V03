7a00da67b58db24a1e0bd2ce8305d96c8365d92e
Sebastián Urbina <sau2113@columbia.edu>
2024-01-24 19:28:44 -0500

    experiment python

---
+++ experiment.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 24 19:26:39 2024
+
+@author: saurbina
+"""
+
+#======================= WEEK Nº2 ================================
+x = 7 + 8 
+y = x ** 2 
+print(y)
+
